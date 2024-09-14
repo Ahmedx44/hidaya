6ab5019273d12ad2839081967377b13c5e6f6a10
Ahmed Gemechu <ahmedgemechu14@gmail.com>
2024-09-14 13:44:27 +0300

    nothing

--- lib/presentation/quran/page/quran.dart
+++ lib/presentation/quran/page/quran.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_islamic_icons/flutter_islamic_icons.dart';
 import 'package:quran_flutter/enums/quran_language.dart';
 import 'package:quran_flutter/models/verse.dart';
 import 'package:quran_flutter/quran.dart';
@@ -12,28 +13,66 @@ class QuranPage extends StatefulWidget {
 }
 
 class _QuranPageState extends State<QuranPage> {
-  var surahVerse;
+  late List<Verse> surahVerse;
 
-  List<Verse> getSurahverse() {
-    final result = Quran.getSurahVersesAsList(widget.surahNumber,
-        language: QuranLanguage.amharic);
-    return result;
+  List<Verse> getSurahVerse() {
+    return Quran.getSurahVersesAsList(widget.surahNumber);
   }
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    final quran = getSurahverse();
-    print(quran[widget.surahNumber].text);
-    surahVerse = quran;
+    surahVerse = getSurahVerse();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text(surahVerse[1].text),
-    ));
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+          child: _showAllVerses(surahVerse, context),
+        ),
+      ),
+    );
   }
+}
+
+Widget _showAllVerses(List<Verse> surah, BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: surah
+        .asMap()
+        .map((index, verse) {
+          return MapEntry(
+            index,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      verse.text,
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Uthmanic',
+                        height: 1.5,
+                      ),
+                    ),
+                  ),
+                  Icon(FlutterIslamicIcons.allah,
+                      color: Theme.of(context).colorScheme.inversePrimary,
+                      size: 24), // Icon after each verse
+                ],
+              ),
+            ),
+          );
+        })
+        .values
+        .toList(),
+  );
 }
